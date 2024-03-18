f797e1676ac723cae5f22aa21a276f03e3e4c09e
Indra Mahesa <indramahesa128@gmail.com>
2024-03-18 10:38:47 +0700

    feat: add vscode launch setting

--- src/game/abstract/FiniteStateMachine.cs
+++ src/game/abstract/FiniteStateMachine.cs
@@ -4,48 +4,50 @@ using Godot.Collections;
 
 public partial class FiniteStateMachine : Node
 {
-	[Export]
-	public NodePath initialState;
-	
-	private State _currentState;
-	private Dictionary<string, State> _states;
+    [Export]
+    public NodePath initialState;
+
+    private State _currentState;
+    private Dictionary<string, State> _states;
 
     public override void _Ready()
     {
-		_states = new Dictionary<string, State>();
-		foreach(Node node in GetChildren())
-		{
-			if(node is State s)
-			{
-				_states[node.Name] = s;
-				s.fsm = this;
-				s.Ready();
-				s.Exit();
-			}
-		}
+        _states = new Dictionary<string, State>();
 
-		_currentState = GetNode<State>(initialState);
-		_currentState.Enter();
+
+        foreach (Node node in GetChildren())
+        {
+            if (node is State s)
+            {
+                _states[node.Name] = s;
+                s.fsm = this;
+                s.Ready();
+                s.Exit();
+            }
+        }
+
+        _currentState = GetNode<State>(initialState);
+        _currentState.Enter();
     }
 
-	public void TransitionTo(String key)
-	{
-		if(!_states.ContainsKey(key) || _currentState == _states[key]) 
-			return;
+    public void TransitionTo(String key)
+    {
+        if (!_states.ContainsKey(key) || _currentState == _states[key])
+            return;
 
-		_currentState.Exit();
-		_currentState = _states[key];
-		_currentState.Enter();
-	}
+        _currentState.Exit();
+        _currentState = _states[key];
+        _currentState.Enter();
+    }
 
     public override void _Process(double delta)
     {
-		_currentState.Update(delta);
+        _currentState.Update(delta);
     }
 
     public override void _PhysicsProcess(double delta)
     {
-		_currentState.PhysicsUpdate(delta);
+        _currentState.PhysicsUpdate(delta);
     }
     public override void _UnhandledInput(InputEvent @event)
     {
